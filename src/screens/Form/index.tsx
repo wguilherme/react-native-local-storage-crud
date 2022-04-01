7ffe6b224f6d52e97acb5ec9b698eaf7f20b1a58
W. Guilherme <wguilhermecriativo@gmail.com>
2022-03-31 23:01:02 -0300

    chore: add async-storage + add handle create todo basic function

--- src/screens/Form/index.tsx
+++ src/screens/Form/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
+import uuid from 'react-native-uuid';
 import { Button } from '../../components/Button';
 import { HeaderForm } from '../../components/HeaderForm';
 import { Input } from '../../components/Input';
@@ -9,7 +10,20 @@ import { styles } from './styles';
 
 export function Form() {
 
+  const [description, setDescription] = useState("")
 
+  function handleCreateTodo() {
+
+    const todo = {
+      id: uuid.v4(),
+      description
+    }
+
+    console.log('todo', todo)
+
+  }
+
+  console.log('description', description)
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -23,12 +37,15 @@ export function Form() {
           <View style={styles.form}>
             <Input
               label="Descrição da tarefa"
+              onChangeText={setDescription}
+              value={description}
             />
           </View>
 
           <View style={styles.footer}>
             <Button
-              title="Salvar"
+              title="Salvar tarefa"
+              onPress={handleCreateTodo}
             />
           </View>
         </ScrollView>
